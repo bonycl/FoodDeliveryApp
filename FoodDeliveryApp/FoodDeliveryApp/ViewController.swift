905040352a67cf43a1b8171131e74c6ee1b5434b
Dinar Shakirov <dinarshakirovdev@gmail.com>
2024-01-30 19:51:01 +0700

    Create base project and remove storyboards ref

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Dinar Shakirov on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
