67eb8eba70eda04e6ec94fbfbc28784e8db08b97
Dinar Shakirov <dinarshakirovdev@gmail.com>
2024-02-04 15:07:14 +0700

    Add other coordinators and tabBarController

---
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Controllers/TabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarController.swift
+//  FoodDeliveryApp
+//
+//  Created by Dinar Shakirov on 04.02.2024.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    init(tabBarControllers: [UIViewController]) {
+        super.init(nibName: nil, bundle: nil)
+        for tabBar in tabBarControllers {
+            self.addChild(tabBar)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.backgroundColor = AppColors.backgroundColor
+        tabBar.tintColor = AppColors.accentOrange
+        
+        
+    }
+}
--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/AppCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/AppCoordinator.swift
@@ -11,7 +11,7 @@ class AppCoordinator: Coordinator {
     
     override func start() {
         print("AppCoordinator start")
-        showOnboardingFlow()
+        showMainFlow()
     }
     
     override func finish() {
@@ -29,7 +29,42 @@ private extension AppCoordinator {
     }
     
     func showMainFlow() {
+        guard let navigationController = navigationController else { return }
         
+        let homeNavigationController = UINavigationController()
+        let homeCoordinator = HomeCoordinator(type: .home, navigationController: homeNavigationController)
+        homeNavigationController.tabBarItem = UITabBarItem(title: "Home", image: UIImage.init(systemName: "house.fill"), tag: 0)
+        homeCoordinator.finishDelegate = self
+        homeCoordinator.start()
+        
+        let orderNavigationController = UINavigationController()
+        let orderCoordinator = OrderCoordinator(type: .order, navigationController: orderNavigationController)
+        orderNavigationController.tabBarItem = UITabBarItem(title: "Order", image: UIImage.init(systemName: "house.fill"), tag: 1)
+        orderCoordinator.finishDelegate = self
+        orderCoordinator.start()
+        
+        let listNavigationController = UINavigationController()
+        let listCoordinator = ListCoordinator(type: .list, navigationController: listNavigationController)
+        listNavigationController.tabBarItem = UITabBarItem(title: "My List", image: UIImage.init(systemName: "house.fill"), tag: 2)
+        listCoordinator.finishDelegate = self
+        listCoordinator.start()
+        
+        let profileNavigationController = UINavigationController()
+        let profileCoordinator = ProfileCoordinator(type: .profile, navigationController: profileNavigationController)
+        profileNavigationController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage.init(systemName: "house.fill"), tag: 3)
+        profileCoordinator.finishDelegate = self
+        profileCoordinator.start()
+        
+        addChildCoordinator(homeCoordinator)
+        addChildCoordinator(orderCoordinator)
+        addChildCoordinator(listCoordinator)
+        addChildCoordinator(profileCoordinator)
+        //collect all  - to array
+        let tabBarControllers = [homeNavigationController, orderNavigationController, listNavigationController, profileNavigationController]
+        
+        let tabBarController = TabBarController(tabBarControllers: tabBarControllers)
+        
+        navigationController.pushViewController(tabBarController, animated: true)
     }
 }
 
